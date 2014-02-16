f77a502b6b2f2879d4ef98a304ce4ddfa04d88dd
Matthew M. Keeler <keelerm84@gmail.com>
2014-02-16 09:07:31 -0500

    Fix clojure-test key bindings and colors

--- customizations/setup-clojure-test.el
+++ customizations/setup-clojure-test.el
@@ -1,7 +1,16 @@
 (require 'cider)
 
-(local-set-minor-mode-key 'clojure-test-mode (kbd "C-c k") nil)
+(set-face-background 'clojure-test-failure-face "dark red")
+(set-face-foreground 'clojure-test-failure-face "ivory")
+(set-face-underline-p 'clojure-test-failure-face nil)
+(set-face-background 'clojure-test-error-face "maroon")
+(set-face-foreground 'clojure-test-error-face "ivory")
+(set-face-underline-p 'clojure-test-error-face nil)
+(set-face-background 'clojure-test-success-face "DarkOliveGreen4")
+(set-face-foreground 'clojure-test-success-face "ivory")
+(set-face-underline-p 'clojure-test-success-face nil)
 
-(local-set-minor-mode-key 'clojure-test-mode (kbd "C-c c") 'clojure-test-clear)
+(define-key clojure-test-mode-map (kbd "C-c k") nil)
+(define-key clojure-test-mode-map (kbd "C-c c") 'clojure-test-clear)
 
 (provide 'setup-clojure-test)
