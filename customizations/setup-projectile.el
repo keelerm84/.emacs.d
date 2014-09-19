2d8c768c44b5bf1957ee1dd87b26b31070d9f125
Matthew M. Keeler <keelerm84@gmail.com>
2014-09-18 22:37:15 -0400

    No point in doing anything if we're not in a project

--- customizations/setup-projectile.el
+++ customizations/setup-projectile.el
@@ -1,11 +1,12 @@
 (setq projectile-enable-caching t)
 
 (defadvice delete-file (before purge-from-projectile-cache (filename &optional trash))
-  (let* ((true-filename (file-truename filename))
-         (relative-filename (file-relative-name true-filename (projectile-project-root))))
-    (if (projectile-project-p)
-        (if (projectile-file-cached-p relative-filename (projectile-project-root))
-            (projectile-purge-file-from-cache relative-filename)))))
+  (if (projectile-project-p)
+      (progn
+        (let* ((true-filename (file-truename filename))
+               (relative-filename (file-relative-name true-filename (projectile-project-root))))
+          (if (projectile-file-cached-p relative-filename (projectile-project-root))
+              (projectile-purge-file-from-cache relative-filename))))))
 
 (ad-activate 'delete-file)
 
