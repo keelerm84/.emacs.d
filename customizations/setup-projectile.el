8c0dd68d9538583449cd582333a95051a8c5b7eb
Matthew M. Keeler <keelerm84@gmail.com>
2014-10-08 20:49:58 -0400

    Cache invalidation no longer necessary
    This defadvice was merged into projectile proper

--- customizations/setup-projectile.el
+++ customizations/setup-projectile.el
@@ -1,13 +1,3 @@
 (setq projectile-enable-caching t)
 
-(defadvice delete-file (before purge-from-projectile-cache (filename &optional trash))
-  (if (projectile-project-p)
-      (progn
-        (let* ((true-filename (file-truename filename))
-               (relative-filename (file-relative-name true-filename (projectile-project-root))))
-          (if (projectile-file-cached-p relative-filename (projectile-project-root))
-              (projectile-purge-file-from-cache relative-filename))))))
-
-(ad-activate 'delete-file)
-
 (provide 'setup-projectile)
