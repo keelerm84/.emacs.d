792bb19c9ad5fed0a29b6f2b640f059a7bfe338e
Matthew M. Keeler <keelerm84@gmail.com>
2014-10-12 05:46:57 -0400

    Cleanup some whitespace

--- customizations/setup-twitter.el
+++ customizations/setup-twitter.el
@@ -1,10 +1,8 @@
 (add-hook 'twittering-edit-mode-hook 'flyspell-mode)
-(add-hook 'twittering-mode 
+(add-hook 'twittering-mode
           (lambda()
             (flyspell-mode t)
             (setq truncate-lines t)
             (setq word-wrap t)))
-                             
 
 (provide 'setup-twitter)
-
