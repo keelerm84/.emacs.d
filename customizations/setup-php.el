077c86b7864e5e5f358812cc30ba78766c6e7fd2
Matthew M. Keeler <keelerm84@gmail.com>
2014-12-23 14:27:36 -0500

    Fix path to php-auto-yasnippets
    The path changed when I moved to using cask instead of package.el

--- customizations/setup-php.el
+++ customizations/setup-php.el
@@ -2,7 +2,7 @@
 
 (setq php-auto-yasnippet-php-program (car
                                       (file-expand-wildcards
-                                       (concat dotfiles-dir "elpa/php-auto-yasnippets-*/Create-PHP-YASnippet.php"))))
+                                       (concat dotfiles-dir ".cask/*/elpa/php-auto-yasnippets-*/Create-PHP-YASnippet.php"))))
 
 (define-key php-mode-map (kbd "C-c C-y") 'yas/create-php-snippet)
 (define-key php-mode-map (kbd "C-c s n") 'sort-php-namespaces)
