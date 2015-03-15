2e1ffa0e99743271dd3f34a2164c7e5f56fb8b83
Matthew M. Keeler <keelerm84@gmail.com>
2015-03-14 22:09:59 -0400

    Try a new way of using hippie expand

--- misc/behavior.el
+++ misc/behavior.el
@@ -19,7 +19,17 @@
 
 (setq twittering-use-master-password t)
 
-(add-to-list 'hippie-expand-try-functions-list 'try-expand-line-all-buffers)
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
 
 ;; Global Modes
 (show-paren-mode 1)
