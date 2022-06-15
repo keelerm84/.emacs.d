d17a84177c8ec4eb7b0453178da4ad98073c44bc
Matthew M. Keeler <keelerm84@gmail.com>
2022-06-14 23:49:50 -0400

    Add company mode

--- init.el
+++ init.el
@@ -396,6 +396,20 @@
 
 (use-package helm-projectile)
 
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+	      ("<tab>" . company-complete-selection))
+  (:map lsp-mode-map
+	("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
