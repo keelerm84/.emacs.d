cc3d2d2738d94b36eaccdf7b7ce609dff1657337
Matthew M. Keeler <keelerm84@gmail.com>
2022-06-15 01:16:00 -0400

    Remove extra line

--- init.el
+++ init.el
@@ -448,8 +448,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(helm-minibuffer-history-key "M-p")
- '(package-selected-packages
-   '(rg which-key visual-fill-column use-package rustic projectile-rails no-littering neotree lsp-ui lsp-treemacs lsp-haskell helm-projectile haskell-mode general forge exec-path-from-shell evil-surround evil-collection doom-themes doom-modeline csharp-mode company-box auto-package-update all-the-icons))
  '(safe-local-variable-values '((lsp-csharp-solution-file . "./NxCoreToPubSub.sln"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
